e69976174a461f43ee81b30346b5047b164aa1ee
Prashant <prashantjh89@gmail.com>
2020-05-28 19:02:06 -0500

    Creating components CommentDetail & Approval Card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return (
+<div className="ui container comments">
+    <ApprovalCard>
+        <CommentDetail 
+        author ="Sam" timeAgo="Today at 6:00PM" 
+        text = "Hi !!"
+        avatar={Faker.image.avatar()}
+        />
+        </ApprovalCard>
+        <ApprovalCard> <CommentDetail author ="Alex" timeAgo="Today at 5:00PM" text = "How are you??" avatar={Faker.image.avatar()}/>
+        </ApprovalCard>
+        <ApprovalCard>
+        <CommentDetail author ="Jen" timeAgo="Yesterday at 6:00PM" text="What's up!" avatar={Faker.image.avatar()}></CommentDetail>
+        </ApprovalCard>
+</div>
+        );
+}
+
+ReactDOM.render(<App></App> , document.querySelector("#root"));
+
